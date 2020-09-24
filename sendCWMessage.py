8902a2a3da96c149c88c3635ae812e16f4ad6915
j_park <>
2020-09-24 16:37:00 +0900

    업데이트

--- sendCWMessage.py
+++ sendCWMessage.py
@@ -7,16 +7,14 @@ import datetime
 import requests
 import pprint
 
-# APIKEY = os.getenv('APIKEY')
-APIKEY = 'fd03c00e9c83ef697353bee32a3c58ab'
-APIKEY2 = os.environ.get('APIKEY')
+APIKEY = os.environ.get('APIKEY')
 
 ENDPOINT = 'https://api.chatwork.com/v2'
 ROOMID = '118327104' # My Chat
 
 post_message_url = f'{ENDPOINT}/rooms/{ROOMID}/messages'
 
-message = 'APIKEY2 : ' + str(APIKEY2) + '\n' + '지금 시간 : ' + str(datetime.datetime.now())
+message = 'APIKEY : ' + str(APIKEY) + '\n' + '지금 시간 : ' + str(datetime.datetime.now())
 
 headers = { 'X-ChatWorkToken': APIKEY }
 params = { 'body': message }
